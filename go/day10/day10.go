deca5ef52feae0162df3600a527e89f978145b47
Sam Bakri <jerry.monclaire@outlook.com>
2024-09-03 13:30:16 -0500

    completed day 10 early; compilation time is so long but it got the job done lol

--- go/day10/day10.go
+++ go/day10/day10.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func day10(input string) (string, int) {
+	result := ""
+	resultCount := 0
+
+	i := 0
+	for i < len(input) {
+		curr := input[i]
+		count := 0
+		for i < len(input) && curr == input[i] {
+			i++
+			count++
+		}
+		result += string(strconv.Itoa(count)) + string(curr)
+		resultCount += 2
+	}
+
+	return result, resultCount
+}
+
+func main() {
+	input := "3113322113"
+	inputCount := 12
+
+	for i := 0; i < 50; i++ {
+		temp, tempCount := day10(input)
+		input = temp
+		inputCount = tempCount
+	}
+	
+
+	fmt.Println(inputCount)
+}
